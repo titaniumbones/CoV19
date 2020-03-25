ce484728153bd65a6fc89b97d82c061258578eb1
EEH <eli.holmes@noaa.gov>
2020-03-25 16:51:00 -0700

    fix wa plot

--- R/walomb_plot.R
+++ R/walomb_plot.R
@@ -1,10 +1,12 @@
-my_plot <- function(x, reg, lockdown){
+my_plot <- function(x, reg, lockdown=NULL){
   p<-plot2(x, reg)
   ymax <- max(subset(x, region==reg)$positive, na.rm=TRUE)
+  if(!missing(lockdown)){
   p <- p + geom_vline(xintercept = as.Date(lockdown)) +
     annotate("text", x=as.Date(lockdown)+0.5, y=.5*ymax, hjust=0, label=paste(reg, "lockdown"), color="red", size=3, angle=90)
   p <- p + geom_vline(xintercept = as.Date(lockdown)+12, linetype = "dashed") +
     annotate("text", x=as.Date(lockdown)+12+0.5, y=.5*ymax, hjust=0, label="12 days past lockdown", color="red", size=3, angle=90)
+  }
   p
 }
 
