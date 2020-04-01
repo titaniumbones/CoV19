fe1efe8a2c0415a2d4c8507c3a6e91884687513d
EEH <eli.holmes@noaa.gov>
2020-04-01 11:36:18 -0700

    add new getworld w states

--- R/getdataworld.R
+++ R/getdataworld.R
@@ -11,15 +11,36 @@ getdataworld <- function(){
     x %>% tidyr::pivot_longer(cols=starts_with("X"),names_to="date", values_to = val)
   }
     url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-    worlddataconfirmed <- try(read.csv(url), silent=TRUE)
-    url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-    worlddatadeaths <- try(read.csv(url), silent=TRUE)
+    worlddataconfirmed <- try(read.csv(url, stringsAsFactor=FALSE), silent=TRUE)
+    url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+    tmp <- try(read.csv(url, stringsAsFactor=FALSE), silent=TRUE)
     if(inherits(worlddataconfirmed, "try-error") || 
-       inherits(worlddatadeaths, "try-error")){
-      cat("Server error. Data was not downloaded.\n")
+       inherits(tmp, "try-error")){
+      cat("Server error. JH confirmed data was not downloaded.\n")
       return()
     }
-    
+    colnames(tmp)[colnames(tmp) == "Long_"] <- "Long"
+    colnames(tmp) <- stringr::str_replace_all(colnames(tmp), "_", ".")
+    colnames(tmp) <- stringr::str_replace_all(colnames(tmp), "2020", "20")
+    tmp$Province.State <- paste(tmp$Admin2, tmp$Province.State)
+    tmp <- tmp[, colnames(tmp) %in% colnames(worlddataconfirmed)]
+    worlddataconfirmed <- dplyr::bind_rows(worlddataconfirmed, tmp)
+    url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    worlddatadeaths <- try(read.csv(url, stringsAsFactor=FALSE), silent=TRUE)
+    url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+    tmp <- try(read.csv(url, stringsAsFactor=FALSE), silent=TRUE)
+    if(inherits(worlddatadeaths, "try-error") || 
+       inherits(tmp, "try-error")){
+      cat("Server error. JH deaths data was not downloaded.\n")
+      return()
+    }
+    colnames(tmp)[colnames(tmp) == "Long_"] <- "Long"
+    colnames(tmp) <- stringr::str_replace_all(colnames(tmp), "_", ".")
+    colnames(tmp) <- stringr::str_replace_all(colnames(tmp), "2020", "20")
+    tmp$Province.State <- paste(tmp$Admin2, tmp$Province.State)
+    tmp <- tmp[, colnames(tmp) %in% colnames(worlddatadeaths)]
+    worlddatadeaths <- dplyr::bind_rows(worlddatadeaths, tmp)
+
     worlddata <- cbind(getx(worlddataconfirmed,"positive"),
                        death=getx(worlddatadeaths,"death")$death)
     
@@ -34,11 +55,21 @@ getdataworld <- function(){
     for(reg in c("China", "Canada")){
     x <- world %>% 
       subset(str_detect(region, reg) & !(region==reg)) %>%
-      group_by(date) %>%
-      summarize_if(is.numeric, sum, na.rm=TRUE)
+      dplyr::group_by(date) %>%
+      dplyr::summarize_if(is.numeric, function(x){ifelse(all(is.na(x)), NA, sum(x, na.rm=TRUE))})
     # This will not have the region column, so we add that back on
     x$region <- reg
     world <- rbind(world, x)
+    }
+    # Create merged set for states
+    for(reg in paste(state.name, "US")){
+      x <- world %>% 
+        subset(str_detect(region, reg) & !(region==reg)) %>%
+        dplyr::group_by(date) %>%
+        dplyr::summarize_if(is.numeric, function(x){ifelse(all(is.na(x)), NA, sum(x, na.rm=TRUE))})
+      # This will not have the region column, so we add that back on
+      x$region <- reg
+      world <- rbind(world, x)
     }
     
     cat("Success! Data downloaded.\n")
